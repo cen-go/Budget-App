31b534aab18552dd031af4d5fcfb8d564189ea87
luzumsuzadam <cengizulutas123123@gmail.com>
2024-06-19 16:41:16 +0300

    selected DOM Elements

--- assets/scripts/budget.js
+++ assets/scripts/budget.js
@@ -3,7 +3,21 @@ const balanceEl = document.querySelector(".budget-header .balance .value");
 const incomeTotalEl = document.querySelector(".income-total");
 const outcomeTotalEl = document.querySelector(".outcome-total");
 const chartEl = document.querySelector(".chart");
+const expenseEl = document.getElementById("expense");
+const incomeEl = document.getElementById("income");
+const allEl = document.getElementById("all");
+const expenseList = document.querySelector("#expense .list");
+const incomeList = document.querySelector("#income .list");
+const allList = document.querySelector("#all .list");
 // SELECT TABS
 const expenseBtn = document.querySelector(".tab1");
 const incomeBtn = document.querySelector(".tab2");
 const allBtn = document.querySelector(".tab3");
+// SELECT INPUT ELEMENTS
+const incomeTitle = document.getElementById("income-title-input");
+const incomeAmount = document.getElementById("income-amount-input");
+const addIncomeBtn = document.querySelector(".add-income");
+const expenseTitle = document.getElementById("expense-title-input");
+const expenseAmount = document.getElementById("expense-amount-input");
+const addExpenseBtn = document.querySelector(".add-expense");
+
