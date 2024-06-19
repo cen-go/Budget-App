2bfb58180c348bb6a7320c1adf22f57a1dbecc58
luzumsuzadam <cengizulutas123123@gmail.com>
2024-06-19 16:25:37 +0300

    js beginnigng

---
+++ assets/scripts/budget.js
@@ -0,0 +1,9 @@
+// SELECT ELEMENTS
+const balanceEl = document.querySelector(".budget-header .balance .value");
+const incomeTotalEl = document.querySelector(".income-total");
+const outcomeTotalEl = document.querySelector(".outcome-total");
+const chartEl = document.querySelector(".chart");
+// SELECT TABS
+const expenseBtn = document.querySelector(".tab1");
+const incomeBtn = document.querySelector(".tab2");
+const allBtn = document.querySelector(".tab3");
