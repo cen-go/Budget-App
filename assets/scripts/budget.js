2d3dc784c92edbd4e971068e2ce5ba2e0195fb7c
luzumsuzadam <cengizulutas123123@gmail.com>
2024-06-20 02:22:41 +0300

    dashboard ui update

--- assets/scripts/budget.js
+++ assets/scripts/budget.js
@@ -21,6 +21,8 @@ const expenseTitle = document.getElementById("expense-title-input");
 const expenseAmount = document.getElementById("expense-amount-input");
 const addExpenseBtn = document.querySelector(".add-expense");
 
+let ENTRY_LIST = [];
+
 // FUNCTIONS FOR SWITCHING TABS
 
 const active = (element) => {
@@ -37,6 +39,60 @@ const show = (element) => {
 
 const hide = (elementsArray) => {
   elementsArray.forEach(element => element.classList.add("hide"));
+};
+
+const clearFields = (inputFields) => {
+  inputFields.forEach(field => field.value = "");
+};
+
+// FUNCTION UPDATING DASHBORD UI
+const addToUI = (type, listEntry) => {  
+    const uiInput = document.createElement("li");
+    uiInput.id = `${ENTRY_LIST.indexOf(listEntry)}`;
+    uiInput.className = `${type}`;
+    uiInput.innerHTML = `
+      <div class="entry">${listEntry.type}: \$${listEntry.amount}</div>
+      <div id="edit"></div>
+      <div id="delete"></div>      
+    `;
+    if (type === "expense") {
+      expenseList.append(uiInput);
+    } else {
+      incomeList.append(uiInput);
+    }
+    const allUiInput = uiInput.cloneNode(true);
+    allList.append(allUiInput);
+};
+
+
+const addExpenseHandler = () => {
+  if (!expenseTitle.value || !expenseAmount.value) {
+    return;
+  }
+  let expense = {
+    type: "expense",
+    title: expenseTitle.value,
+    amount: parseFloat(expenseAmount.value),
+  }
+  ENTRY_LIST.push(expense);
+  console.log(ENTRY_LIST);
+  addToUI(expense.type, expense);
+  clearFields([expenseTitle, expenseAmount]);
+};
+
+const addIncomeHandler = () => {
+  if (!incomeTitle.value || !incomeAmount.value) {
+    return;
+  }
+  let income = {
+    type: "income",
+    title: incomeTitle.value,
+    amount: parseFloat(incomeAmount.value),
+  }
+  ENTRY_LIST.push(income);
+  console.log(ENTRY_LIST);
+  addToUI(income.type, income);
+  clearFields([incomeTitle, incomeAmount]);
 };
 
 // EVENT LISTENERS FOR TABS
@@ -61,3 +117,9 @@ allBtn.addEventListener("click", () => {
   show(allEl);
   hide([expenseEl, incomeEl]);
 });
+
+// EVENT LISTENERS FOR INPUTS
+
+addExpenseBtn.addEventListener("click", addExpenseHandler);
+
+addIncomeBtn.addEventListener("click", addIncomeHandler);
